58a86ad201332e9ddcd47dea66a43afdfb9d3701
Biplab1998 <bittupaul1998@gmail.com>
2025-11-02 11:18:36 +0530

    add request and connections page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,33 @@
-import { useSelector } from "react-redux";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+import { removeUser } from "../utils/userSlice";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
-  console.log(user);
+  const dispatch = useDispatch();
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(BASE_URL + "/logout", {}, { withCredentials: true });
+      dispatch(removeUser());
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">👩‍💻 DevConnect</a>
+        <Link to="/" className="btn btn-ghost text-xl">
+          👩‍💻 DevConnect
+        </Link>
       </div>
       {user && (
         <div className="flex-none gap-2">
           <div className="form-control">Welcome, {user.firstName}</div>
-          <div className="dropdown dropdown-end mx-5 flex">
+          <div className="dropdown dropdown-end mdx-5 flex">
             <div
               tabIndex={0}
               role="button"
@@ -27,16 +42,23 @@ const NavBar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
-                <a>Settings</a>
+                <Link to="/connections" className="justify-between">
+                  Connections
+                </Link>
               </li>
               <li>
-                <a>Logout</a>
+                <Link to="/requests" className="justify-between">
+                  Requests
+                </Link>
+              </li>
+              <li>
+                <a onClick={handleLogout}>Logout</a>
               </li>
             </ul>
           </div>
